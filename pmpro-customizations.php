78b2c4c49ec86e0acab7b4d37a5ecadde281360d
Larry Kagan <larry@superiocity.com>
2016-04-26 11:22:22 -0400

    Customized for FLT.

--- pmpro-customizations.php
+++ pmpro-customizations.php
@@ -1,56 +1,78 @@
 <?php
 /*
-Plugin Name: Register Helper Example
+Plugin Name: Paid Membership Pro Customizations
 Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-customizations/
-Description: Register Helper Initialization Example
-Version: .1
-Author: Stranger Studios
-Author URI: http://www.strangerstudios.com
+Description: Customizations to the Membership Functionality
+Version: 1.0
+Author: Superiocity
+Author URI: http://www.superiocity.com
 */
-//we have to put everything in a function called on init, so we are sure Register Helper is loaded
-function my_pmprorh_init()
-{
-    //don't break if Register Helper is not loaded
-    if(!function_exists("pmprorh_add_registration_field"))
-    {
-        return false;
-    }
 
-    //define the fields
-    $fields = array();
-    $fields[] = new PMProRH_Field(
-        "company",              // input name, will also be used as meta key
-        "text",                 // type of field
-        array(
-            "size"=>40,         // input size
-            "class"=>"company", // custom class
-            "profile"=>true,    // show in user profile
-            "required"=>true    // make this field required
-        ));
-    $fields[] = new PMProRH_Field(
-        "referral",                     // input name, will also be used as meta key
-        "text",                         // type of field
-        array(
-            "label"=>"Referral Code",   // custom field label
-            "profile"=>"admins"         // only show in profile for admins
-        ));
-    $fields[] = new PMProRH_Field(
-        "gender",                   // input name, will also be used as meta key
-        "select",                   // type of field
-        array(
-            "options"=>array(       // <option> elements for select field
-                    "" => "",       // blank option - cannot be selected if this field is required
-                "male"=>"Male",     // <option value="male">Male</option>
-                "female"=>"Female"  // <option value="female">Female</option>
-        )));
+namespace Superiocity\FLT;
 
-    //add the fields into a new checkout_boxes are of the checkout page
-    foreach($fields as $field)
-        pmprorh_add_registration_field(
-            "checkout_boxes", // location on checkout page
-            $field            // PMProRH_Field object
-        );
+class PMPro_Customizations {
 
-    //that's it. see the PMPro Register Helper readme for more information and examples.
+	function init()
+	{
+		// don't break if Register Helper is not loaded
+		if ( ! function_exists( '\pmprorh_add_registration_field' ) ) {
+			return false;
+		}
+
+		// define the fields
+		$fields   = array();
+		
+		$fields[] = new \PMProRH_Field(
+			'zip',
+			'text',
+			array(
+				'label'    => 'Zip code',
+				'size'     => 5,
+				'required' => true,
+				'profile'  => true,
+			) );
+
+		$fields[] = new \PMProRH_Field(
+			'gift',
+			'select',
+			array(
+				'label'    => 'Free gift',
+				'required' => true,
+				'profile'  => true,
+				'options'  => array(
+					''      => '',
+					'visor' => 'visor',
+					'hat'   => 'hat',
+				),
+				'levels' => array( 1, 3, 4 ),
+			) );
+		
+		$fields[] = new \PMProRH_Field(
+			'shirt_size',
+			'select',
+			array(
+				'label'   => 'Shirt size',
+				'required' => true,
+				'profile'  => true,
+				'levels' => array( 1, 3 ),
+				'options' => array(
+					''         => '',
+					'x-small'  => 'extra small',
+					'small'    => 'small',
+					'medium'   => 'medium',
+					'large'    => 'large',
+					'x-large'  => 'extra large',
+				)
+			) );
+		
+		//add the fields into a new checkout_boxes are of the checkout page
+		foreach ( $fields as $field ) {
+			pmprorh_add_registration_field(
+				'checkout_boxes', // location on checkout page
+				$field            // PMProRH_Field object
+			);
+		}
+	}
 }
-add_action("init", "my_pmprorh_init");
+
+add_action( 'init', array( 'Superiocity\FLT\PMPro_Customizations', 'init' ) );
